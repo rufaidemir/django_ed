a50c65b0504ed1e743bd5c51f2c362451edbfe8c
Rufai Demirgit config --global user.name "Rufai Demir"git config --global user.name Rufai <rufai.demir.1@gmail.comgit config --global user.email rufai.demir.1@gmail.comgit config --global user.email rufai.demir.1@gmail.com>
2020-01-24 00:41:39 +0300

    Erisilebilir hale gelmis hali

---
+++ page/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+from .models import Page
+
+
+class PageModify(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    list_display = (
+        'pk',
+        'title',
+        'slug',
+        'status', 
+        'updated_at',
+    )
+    list_filter = ('status', )
+    list_editable = (
+        'title',
+        'status', 
+    )
+
+
+admin.site.register(Page, PageModify)
