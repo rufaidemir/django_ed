dcde48570b7d320ed21e7e3a6ab8092d0f48626f
Rufai Demirgit config --global user.name "Rufai Demir"git config --global user.name Rufai <rufai.demir.1@gmail.comgit config --global user.email rufai.demir.1@gmail.comgit config --global user.email rufai.demir.1@gmail.com>
2020-01-24 23:09:41 +0300

    Some kind of template setting had added

--- page/views.py
+++ page/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-
+from .models import Courusel
 def index(request):
     context = dict()
+    carusel_list = Courusel.objects.all()
+    context['carusel_list'] = carusel_list
     return render(request, 'home/index.html', context)
+
